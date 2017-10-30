9038d263d74f94ce782b4036f12a1c1a4a87efb2
RubyDong <109739467@mm45.local>
2017-10-30 15:13:31 -0400

    included responsive website

---
+++ website/js/javascript.js
@@ -0,0 +1,6 @@
+ $(document).ready(function(){
+     $("#navlinks a").click(function() {
+        $("#navlinks a").css({"color":"#B3D7E8", "background-color": "transparent"});
+        $(this).css({"color": "#fff", "background-color": "#B3D7E8"});
+    });
+});
